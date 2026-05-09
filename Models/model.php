135466f406970901f1adab4670905f407814a949
huytrg755 <huyquangtruong755@gmail.com>
2026-05-09 15:43:07 +0700

    update function limit, sanpham_danhmuc

--- Models/model.php
+++ Models/model.php
@@ -3,17 +3,20 @@ require_once("connection.php");
 class model
 {
     var $conn;
+        //Tự động tạo kết nối database khi object Model được khởi tạo
     function __construct()
     {
         $conn_obj = new connection();
         $this->conn = $conn_obj->conn;
     }
+    //lấy dữ liệu danhmuc, sanpham, banner, random, phantrang và loctheodanhmuc thoong qua inline SQL
     function limit($a, $b)
     {
+        //hàm limit($a,$b) chỉ lấy sản phẩm đang "bật/bán", sắp xếp mới nhất trước, phân trang (lấy từ vị trí $a, lấy $b dòng)
         $query =  "SELECT * from sanpham WHERE TrangThai = 1  ORDER BY ThoiGian DESC limit $a,$b";
 
         require("result.php");
-
+        //mảng sản phẩm
         return $data;
     }
     function danhmuc()
@@ -59,6 +62,7 @@ class model
     }
     function sanpham_danhmuc($a, $b, $danhmuc)
     {
+        //lọc theo danh mục, ưu tiên thời gian mới nhất + phân trang
         $query =   "SELECT * from sanpham WHERE TrangThai = 1  and MaDM = $danhmuc ORDER BY ThoiGian DESC limit $a,$b";
 
         require("result.php");
